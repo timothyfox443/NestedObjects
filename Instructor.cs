7e4aafd226bd699a8b9e38d10769f38346b2d23c
Timothy Fox <timothyfox443@gmail.com>
2021-07-21 18:37:20 -0700

    added instructor class - incomplete

---
+++ Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// legal fullname - FirstLastIE JoeOrtiz
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// contact email for instructor IE john.doe@school.edu
+        /// </summary>
+        public string Email { get; set; }
+       
+    }
+}
